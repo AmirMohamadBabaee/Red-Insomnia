548233e292de93856309f72b68cf0ace75d51208
9831011 <9831011@aut.ac.ir>
2020-05-11 15:15:37 +0430

    UPDATE: Complete adding startAgain method to restart frame

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,10 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("\uD83D\uDDC1");
-        new MainFrame("RedInsomnia", MainFrame.DARK_THEME);
+        new MainFrame("RedInsomnia", MainFrame.LIGHT_THEME);
+    }
+
+    public static void startAgain(int theme) {
+        new MainFrame("RedInsomnia", theme);
     }
 }
