6cba5233f5d5f18026d7704c60909e6695035092
9831011 <9831011@aut.ac.ir>
2020-05-12 16:31:14 +0430

    UPDATE: Complete AboutFrame

--- src/RedInsomnia/gui/AboutFrame.java
+++ src/RedInsomnia/gui/AboutFrame.java
@@ -1,8 +1,12 @@
 package RedInsomnia.gui;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * RedInsomnia.gui.AboutFrame
@@ -29,7 +33,7 @@ public class AboutFrame extends JFrame {
 
         SwingUtilities.updateComponentTreeUI(this);
 
-        setTitle("About Us");
+        setTitle("About Me");
         setSize(800, 500);
         setResizable(false);
         setLocationRelativeTo(mainFrame);
@@ -37,6 +41,45 @@ public class AboutFrame extends JFrame {
         setIconImage(icon);
         setLayout(null);
         setVisible(true);
+
+        setLayout(new BorderLayout());
+
+        BufferedImage myPicture = null;
+        try {
+            myPicture = ImageIO.read(new File(mainFrame.getCurrentDir() + "\\about-me.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        assert myPicture != null;
+        JLabel aboutMePic = new JLabel(new ImageIcon(myPicture));
+        add(aboutMePic, BorderLayout.NORTH);
+
+        JLabel developerName = new JLabel();
+        developerName.setFont(new Font("Santa Fe Let", Font.PLAIN, 20));
+        developerName.setText("Developer: AmirMohamad Babaee (Amir01)");
+        developerName.setAlignmentX(Component.CENTER_ALIGNMENT);
+        developerName.setHorizontalAlignment(JLabel.CENTER);
+
+        JLabel developerID = new JLabel();
+        developerID.setFont(new Font("Santa Fe Let", Font.PLAIN, 20));
+        developerID.setText("Student ID: 9831011");
+        developerID.setAlignmentX(Component.CENTER_ALIGNMENT);
+        developerID.setHorizontalAlignment(JLabel.CENTER);
+
+        JLabel developerEmail = new JLabel();
+        developerEmail.setFont(new Font("Santa Fe Let", Font.PLAIN, 20));
+        developerEmail.setText("Email: amir.babaee79@aut.ac.ir");
+        developerEmail.setAlignmentX(Component.CENTER_ALIGNMENT);
+        developerEmail.setHorizontalAlignment(JLabel.CENTER);
+
+        JPanel centerPanel = new JPanel(new GridLayout(0, 1));
+        centerPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        centerPanel.add(developerName);
+        centerPanel.add(developerID);
+        centerPanel.add(developerEmail);
+
+        add(centerPanel, BorderLayout.CENTER);
 
     }
 
