1ddbb8445a0fcf7506c8b2d2946dfe297effc3f7
9831011 <9831011@aut.ac.ir>
2020-05-12 15:01:55 +0430

    ADD: create HelpFrame class

---
+++ src/RedInsomnia/gui/HelpFrame.java
@@ -0,0 +1,21 @@
+package RedInsomnia.gui;
+
+import javax.swing.*;
+
+
+/**
+ * RedInsomnia.gui.HelpFrame
+ *
+ * This class define new frame for help menuItem
+ *
+ * @author Amir01
+ * @version
+ *
+ * @see MainFrame
+ * @see JFrame
+ */
+public class HelpFrame extends JFrame{
+
+
+
+}
