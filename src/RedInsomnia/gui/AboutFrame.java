e3e965add4ddcb3289647ef9fde2dd7bdac74e54
9831011 <9831011@aut.ac.ir>
2020-05-12 14:57:08 +0430

    ADD: Create AboutFrame class

---
+++ src/RedInsomnia/gui/AboutFrame.java
@@ -0,0 +1,17 @@
+package RedInsomnia.gui;
+
+import javax.swing.*;
+
+/**
+ * RedInsomnia.gui.AboutFrame
+ *
+ * This class define new frame for about menuItem
+ *
+ * @author Amir01
+ * @version
+ *
+ * @see MainFrame
+ * @see JFrame
+ */
+public class AboutFrame extends JFrame {
+}
