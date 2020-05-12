2135d8b728d0f735daa192566c5962a3d1557cbc
9831011 <9831011@aut.ac.ir>
2020-05-12 14:51:41 +0430

    UPDATE: Use SwingUtilities.invokeLater method to make concurrent gui frame also Add this class to RedInsomnia.main packcage

---
+++ src/RedInsomnia/main/Main.java
@@ -0,0 +1,20 @@
+package RedInsomnia.main;
+
+import RedInsomnia.gui.MainFrame;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(() -> new MainFrame("RedInsomnia", MainFrame.LIGHT_THEME));
+
+    }
+
+    public static void startAgain(int theme) {
+
+        SwingUtilities.invokeLater(() -> new MainFrame("RedInsomnia", theme));
+
+    }
+}
