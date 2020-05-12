c184b216e9ab481412623f056364eea747ec981d
9831011 <9831011@aut.ac.ir>
2020-05-12 15:04:35 +0430

    UPDATE: Complete base frame of about and help menuItem

--- src/RedInsomnia/gui/AboutFrame.java
+++ src/RedInsomnia/gui/AboutFrame.java
@@ -1,6 +1,8 @@
 package RedInsomnia.gui;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 
 /**
  * RedInsomnia.gui.AboutFrame
@@ -14,4 +16,28 @@ import javax.swing.*;
  * @see JFrame
  */
 public class AboutFrame extends JFrame {
+
+    public AboutFrame(MainFrame mainFrame, int theme) {
+
+        super();
+
+        try {
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+        } catch (ClassNotFoundException | UnsupportedLookAndFeelException | IllegalAccessException | InstantiationException e) {
+            e.printStackTrace();
+        }
+
+        SwingUtilities.updateComponentTreeUI(this);
+
+        setTitle("About Us");
+        setSize(800, 500);
+        setResizable(false);
+        setLocationRelativeTo(mainFrame);
+        Image icon = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB_PRE);
+        setIconImage(icon);
+        setLayout(null);
+        setVisible(true);
+
+    }
+
 }
--- src/RedInsomnia/gui/HelpFrame.java
+++ src/RedInsomnia/gui/HelpFrame.java
@@ -1,6 +1,8 @@
 package RedInsomnia.gui;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 
 
 /**
@@ -16,6 +18,27 @@ import javax.swing.*;
  */
 public class HelpFrame extends JFrame{
 
+    public HelpFrame(MainFrame mainFrame, int theme) {
 
+        super();
+
+        try {
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+        } catch (ClassNotFoundException | UnsupportedLookAndFeelException | IllegalAccessException | InstantiationException e) {
+            e.printStackTrace();
+        }
+
+        SwingUtilities.updateComponentTreeUI(this);
+
+        setTitle("Help");
+        setSize(800, 500);
+        setResizable(false);
+        setLocationRelativeTo(mainFrame);
+        Image icon = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB_PRE);
+        setIconImage(icon);
+        setLayout(null);
+        setVisible(true);
+
+    }
 
 }
